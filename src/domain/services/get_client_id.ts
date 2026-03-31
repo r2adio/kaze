7a412af1ffe7de4cca09e3f558f2e40d216d1bb9
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-04-01 00:11:58 +0530

    feat: returns the ip address from req-headers

---
+++ src/domain/services/get_client_id.ts
@@ -0,0 +1,17 @@
+import net from "node:net";
+
+// returns the client ip address
+// TODO: add api-key header check -> getClientId()
+export default function getClientIP(req: Request): string | undefined {
+  const forwarded = req.headers.get("x-forwarded-for"); // X-Forwarded-For header
+  if (forwarded) {
+    const ip = forwarded.split(",")[0]?.trim();
+    if (ip && net.isIP(ip)) return ip;
+  }
+  const realIp = req.headers.get("x-real-ip"); // X-Real-IP header
+  if (realIp) {
+    const ip = realIp.trim();
+    if (net.isIP(ip)) return ip;
+  }
+  return undefined;
+}
