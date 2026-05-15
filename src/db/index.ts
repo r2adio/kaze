e736eb11b7ee243a7f4a6bed0baa270590951dc4
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-05-15 23:59:39 +0530

    feat: add drizzle and postgresql setup

---
+++ src/db/index.ts
@@ -0,0 +1,9 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema.js";
+import env from "../env.js";
+
+export const pool = new Pool({ connectionString: env.DATABASE_URL });
+
+export const db = drizzle({ client: pool, schema });
+export type DB = typeof db;
